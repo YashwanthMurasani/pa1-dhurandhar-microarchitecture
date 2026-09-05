15a01ff84ccbffae028f44530e7f19a2f8223789
Yashwanth <saiyashwanthreddymurasani@gmail.com>
2026-09-05 20:59:21 +0530

    reorder completed

--- task1/src/conv_reorder.cpp
+++ task1/src/conv_reorder.cpp
@@ -1,10 +1,23 @@
 // conv_reorder.cpp  STAGE 1: LOOP REORDERING
 // Hint: loops from outermost to innermost -> ky, kx, oy, ox.
-
-#include "convolution.h"
+#include "convolution.h" 
+#include <bits/stdc++.h>
+using namespace std;
 
 void conv_reorder(const float* in, float* out, const float* ker,
-                  int H, int W, int K) {
-    // TODO(student): replace this placeholder with your reordered implementation.
-    conv_naive(in, out, ker, H, W, K);
+                 int H, int W, int K) {
+    const int p = K / 2;
+    const int in_stride = W + 2 * p;  // padded row stride
+
+    for(int idx=0;idx<(H*W);idx++) out[idx]=0.0f; 
+    for (int oy = 0; oy < H; ++oy) {
+        for (int ky = 0; ky < K; ++ky) {
+            for (int kx = 0; kx < K; ++kx) {
+                float temp=ker[ky*K+kx];
+                for (int ox = 0; ox < W; ++ox) {
+                    out[oy * W + ox]+= in[(oy + ky) * in_stride + (ox + kx)] * temp;
+                }
+            }
+        }
+    }
 }
